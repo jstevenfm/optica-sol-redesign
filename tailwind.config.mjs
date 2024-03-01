719b381d73caff10ad5f948c81b815ed6ba51d96
Jeisson Fonseca <jstevenfm@gmail.com>
2024-02-29 21:07:20 -0500

    feat - Added colors and font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,17 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		colors: {
+			'white': '#f3f4f8',
+			'black': '#1a1a1a',
+			'blueOS': {
+				50: '#E1F6FF',
+				400: '#6695D6',
+				500: '#4A81C3',
+				700: '#116098',
+				900: '#003766',
+			}
+		},
 		extend: {},
 	},
 	plugins: [],
